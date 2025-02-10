71d7b69dcfd59ffc3848e5d5e511970c61fad4bb
damian-andrzej <102800704+damian-andrzej@users.noreply.github.com>
2025-02-10 16:49:08 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,9 @@
 # backend.tf
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "path/to/terraform.tfstate"
+    bucket         = "damian-andrzej9-statebucket"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"  # Optional: Enable state locking using DynamoDB
-    acl            = "bucket-owner-full-control"
   }
 }
