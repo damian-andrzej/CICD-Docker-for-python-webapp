2d3008f80b3b02ff39d444d448ab6c4eebc0cdfc
damian-andrzej <102800704+damian-andrzej@users.noreply.github.com>
2025-02-11 11:40:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,10 @@ resource "aws_internet_gateway" "igw" {
 # Create a Route Table
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 # Associate Route Table with the Subnet
